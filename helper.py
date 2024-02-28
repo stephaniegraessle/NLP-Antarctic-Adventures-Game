623a0539fc86a145cfb5a57bb34ada39e89cb6fb
Stephanie <stephanie.graessle@tuni.fi>
2024-02-28 20:35:09 +0200

    Add move function

---
+++ helper.py
@@ -0,0 +1,9 @@
+
+# Input: current location as list [x,y] and direction moving in
+# Output: new location
+def move(loc, dir) :
+    # when east, x+1
+    # when north, y+1
+    # when west, x-1
+    # when south, y-1
+    return loc
