2607171889eecc59992441b4110233d15af770d6
Stephanie <stephanie.graessle@tuni.fi>
2024-03-04 15:15:26 +0200

    Add gen_environ()

--- helper.py
+++ helper.py
@@ -1,3 +1,4 @@
+import random
 
 # Input: current location as list [x,y] and direction moving in
 # Output: new location
@@ -7,3 +8,10 @@ def move(loc, dir) :
     # when west, x-1
     # when south, y-1
     return loc
+
+# Output: A random environment
+def gen_environ():
+    environ_types = ["snow field","ocean"]
+    environ = random.choices(environ_types, weights=(80,20),k=1)[0] # gen environ, more likely to travel to "snow field" than "ocean"
+    # TODO: Generate a random function indicating shoreline. Points on one side of line are snow field, those on the other are the ocean.
+    return environ
