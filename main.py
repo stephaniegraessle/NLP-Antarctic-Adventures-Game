c88e3f2269dd8d9e5d1fa9c732d6341a647d84cd
Stephanie <stephanie.graessle@tuni.fi>
2024-03-04 15:16:01 +0200

    Add initial environment generation and comments with testing commands

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import os
 import pgf
 import process
+import helper
 
 # location events
 import lake
@@ -14,7 +15,10 @@ def main():
     os.system("gf -make AdventureEng.gf") # generate PGF file
     gr = pgf.readPGF("Adventure.pgf") # read PGF file
     lang = gr.languages["AdventureEng"] # get English languge (default)
-    
+    #process.print_grammar_info(gr) # Print info about grammar
+
+    environ = helper.gen_environ() # intialize starting environment
+
     loc = [0,0] # initialize starting location
     inventory = [None] * 10 # create empty inventory of 10 items--change to [None,0]?
     hunger = 100 # initialize full hunger
@@ -24,7 +28,25 @@ def main():
     success = False
     while not quit:
         try:
-            args = process.get_input(lang)
+            
+
+            # program gives information about environment
+
+            args = process.get_input(lang) # get user input
+
+            # determine which type of command the user inputted
+
+            # determine useful information from command, e.g., verb, determiners, objects, etc.
+
+            # do appropriate followup action
+
+
+            # determiner function testing
+            #det = process.get_det(args,gr)
+            #print("Det:",det)
+            #det_amt = process.get_det_amount(det)
+            #print("Det amount:",det_amt)
+
         except:
             #os.system("rm Adventure.pgf") # remove generated PGF file
             exit()
