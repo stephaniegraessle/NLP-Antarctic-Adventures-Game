27b50cec30ec24dea74a8f308b097f28136dddf1
Stephanie <stephanie.graessle@tuni.fi>
2024-03-04 17:58:01 +0200

    Add constant values from constant.py

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import pgf
 import process
 import helper
 
+# constants
+import constant
+
 # location events
 import lake
 
@@ -21,8 +24,8 @@ def main():
 
     loc = [0,0] # initialize starting location
     inventory = [None] * 10 # create empty inventory of 10 items--change to [None,0]?
-    hunger = 100 # initialize full hunger
-    hp = 100 # initialize full hp
+    hunger = constant.MAX_HUNGER # initialize full hunger
+    hp = constant.MAX_HP # initialize full hp
 
     quit = False
     success = False
@@ -35,6 +38,7 @@ def main():
             args = process.get_input(lang) # get user input
 
             # determine which type of command the user inputted
+            #type = process.get_type(args)
 
             # determine useful information from command, e.g., verb, determiners, objects, etc.
 
@@ -47,6 +51,8 @@ def main():
             #det_amt = process.get_det_amount(det)
             #print("Det amount:",det_amt)
 
+            #hunger = helper.lose_hunger(hunger,5)
+            #hp = helper.hunger_check(hunger,hp)
         except:
             #os.system("rm Adventure.pgf") # remove generated PGF file
             exit()
